9fdea361fa9324fe079e9cceea86bec112ec59dd
xz-dice <xandicearmah@gmail.com>
2021-03-31 16:16:15 +0100

    made changes according to code review

--- index.php
+++ index.php
@@ -13,7 +13,6 @@ $songsToDisplay = displaySongs($songs);
     <link rel="stylesheet" href="normalize.css" />
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Roboto:wght@400;700&Work+Sans:wght@400;600;700;900&display=swap" rel="stylesheet">
-<!--    <link rel="icon" href="x.png" type="image/icon type">-->
     <meta name=“viewport” content=“width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css" type="text/css" />
 </head>
@@ -34,7 +33,6 @@ $songsToDisplay = displaySongs($songs);
         <th>Artist</th>
         <th>Album</th>
         <th>Artwork</th>
-<!--        <th>Track Preview</th>-->
         </thead>
         <tbody>
         <?php echo $songsToDisplay; ?>
